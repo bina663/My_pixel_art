90a730339e64158d45a6b297e8442e2a7fbcb89b
Sabrina <bina.oliver4@gmail.com>
2023-07-20 19:59:36 -0300

    Formatt text

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,10 +7,7 @@
     <div class="div-about">
         <h2>Sobre</h2>
         <section>
-            <p>Ei, bem-vindo ao meu site de pixel art! Aqui você pode conferir todas as minhas criações incríveis feitas com pequenos e coloridos blocos de pixels.
-                Deixei minha imaginação correr solta em cada obra, e tenho muito orgulho de mostrar como evoluí ao longo do tempo. Desde os primeiros desenhos até as peças mais recentes, sempre me esforcei para melhorar e dar vida a cada pixel.
-                É muito legal compartilhar minha paixão por pixel art com você! Espero que você encontre inspiração e se divirta enquanto explora minhas criações. Sinta-se à vontade para deixar seus comentários, porque eles significam muito para mim.
-                Então, puxe uma cadeira virtual, dê uma olhada na galeria e mergulhe no mundo mágico dos pixels! Obrigado por estar aqui e aproveitar essa jornada artística comigo.</p>
+            <p>Ei, bem-vindo ao meu site de pixel art! Aqui você pode conferir todas as minhas criações incríveis feitas com pequenos e coloridos blocos de pixels. Deixei minha imaginação correr solta em cada obra, e tenho muito orgulho de mostrar como evoluí ao longo do tempo. Desde os primeiros desenhos até as peças mais recentes, sempre me esforcei para melhorar e dar vida a cada pixel. É muito legal compartilhar minha paixão por pixel art com você! Espero que você encontre inspiração e se divirta enquanto explora minhas criações.</p>
         </section>
     </div>
         
